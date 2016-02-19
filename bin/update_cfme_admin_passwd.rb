b9cbb720230425ed5824aa6e870dad0c2cc83901
jesus m. rodriguez <jesusr@redhat.com>
2016-02-19 11:21:36 -0500

    rubocop: Use ! instead of not, trailing blank line

--- bin/update_cfme_admin_passwd.rb
+++ bin/update_cfme_admin_passwd.rb
@@ -6,9 +6,8 @@ require miq_environment
 user_name = "admin"
 
 admin_user = User.find_by_userid(user_name)
-if not admin_user
+if !admin_user
   puts "Unable to find user: #{user_name}"
   exit 1
 end
 admin_user.change_password('smartvm', ARGV[0])
-
